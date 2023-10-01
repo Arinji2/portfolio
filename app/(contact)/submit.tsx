f219268712fe822fd694cb27e6c88e693dc541d3
Arinjay Dhar <87296817+Arinji2@users.noreply.github.com>
2023-10-01 10:55:48 +0530

    Fix Contrast Issues

--- app/(contact)/submit.tsx
+++ app/(contact)/submit.tsx
@@ -6,7 +6,7 @@ export function SubmitButton() {
       onClick={() => {
         window.dispatchEvent(new Event("mailSent"));
       }}
-      className="w-fit py-3 px-5 h-fit bg-brand-background-secondary disabled:bg-brand-background-primary disabled:text-white text-brand-primary rounded-md"
+      className="w-fit py-3 px-5 h-fit bg-brand-background-secondary text-white rounded-md"
     >
       <p className="text-[20px]">Submit</p>
     </button>
