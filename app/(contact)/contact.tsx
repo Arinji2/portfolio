8b8210e02e9efe868e06e7253a87616e9e80b7f1
Arinjay Dhar <87296817+Arinji2@users.noreply.github.com>
2023-10-01 15:16:32 +0530

    Fix Contact form again

--- app/(contact)/contact.tsx
+++ app/(contact)/contact.tsx
@@ -9,14 +9,14 @@ export default async function Contact() {
   return (
     <section
       tabIndex={0}
-      className="w-full relative h-[100svh] flex flex-col items-center justify-start py-5 px-2"
+      className="w-full relative h-full md:h-[100svh] flex flex-col items-center justify-start py-5 px-2"
     >
       <WidthWrapper transparent>
         <div className="w-full xl:w-[90%] h-full flex flex-col items-start justify-start z-20">
           <h2 className="text-brand-primary font-bold text-center md:text-left text-[50px] md:text-[80px] xl:text-[100px]">
             Contact Me!
           </h2>
-          <div className="w-full  md:w-[95%] h-fit flex-nowrap flex flex-row items-center justify-center md:justify-start  gap-12">
+          <div className="w-full mt-6 md:w-[95%] h-fit flex-nowrap flex flex-row items-center justify-center md:justify-start  gap-12">
             <Link
               href="https://www.github.com/Arinji2"
               target="_blank"
