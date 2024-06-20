5ae65d4755fa5270cdf699c11e4c409a2e4b98c4
Arinji2 <arinjaydhar205@gmail.com>
2024-06-20 22:36:45 +0530

    Update Testemonials

--- app/freelance.tsx
+++ app/freelance.tsx
@@ -13,12 +13,24 @@ export default async function Freelance() {
               <span className="text-brand-primary">Next big thing</span>
             </h2>
           </div>
-          <div className="w-[95%] xl:w-full gap-5 md:pl-10  h-full flex flex-row items-start pt-10 no-scrollbar justify-center md:justify-start overflow-x-scroll z-50">
+          <div className="w-[95%] xl:w-full gap-5 md:pl-10  h-full flex flex-row items-start pt-10 px-1 md:px-0 small-scrollbar justify-start overflow-x-scroll z-50">
+            <TechBlock
+              name="Antonio"
+              description="Committed and dedicated team member. In the short time he has been with our team, he has consistently exceeded our expectations by developing unique and customized enhancements."
+              post="CEO of Revivenode"
+              icon="revive.webp"
+            />
             <TechBlock
               name="Duh"
-              description="Arinji works hard and gets the job done on time, I initially had him join my team to fix up a site, he encouraged me to redesign and recode, he’s done all of that in less than a month or 2."
+              description="Arinji works hard and gets the job done on time, I initially had him join to fix up a site, he encouraged me to redesign and recode, he’s done all of that in a month."
               post="CEO of Feds.lol"
-              icon="feds.png"
+              icon="feds.webp"
+            />
+            <TechBlock
+              name="Wiscers"
+              description="Arinji was an absolute pleasure to work with. exceeded my expectations and very easy going"
+              post="Owner of Vanilla Earth"
+              icon="ve.webp"
             />
           </div>
         </div>
@@ -50,10 +62,10 @@ function TechBlock({
       <div className="w-full h-[40%] flex flex-col items-start justify-start text-left gap-2">
         <div className="w-[90%] h-[2px] bg-white"></div>
         <div className="w-[90%] h-full flex flex-row items-end justify-between">
-          <div className="w-fit h-fit flex flex-col items-start justify-start gap-1">
+          <div className="w-full md:w-fit h-fit flex flex-col items-start justify-start gap-1">
             <p className="text-[30px] font-bold text-brand-primary">{name}</p>
             <div className="w-full h-full flex flex-col items-start text-left justify-center gap-1">
-              <p className="text-[15px] text-white">{post}</p>
+              <p className="text-[10px] md:text-[15px] text-white">{post}</p>
               <div className="w-full h-[2px] bg-white"></div>
             </div>
           </div>
