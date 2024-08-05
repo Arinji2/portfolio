d5e703837078ce4d8da1e46d6adaf11d669d5451
Arinji2 <arinjaydhar205@gmail.com>
2024-08-05 23:05:11 +0530

    Add Turnstile

--- app/(contact)/form.tsx
+++ app/(contact)/form.tsx
@@ -1,12 +1,18 @@
 "use client";
+import { useState } from "react";
+import Turnstile, { useTurnstile } from "react-turnstile";
 import { sendMail } from "./sendMail";
 import { SubmitButton } from "./submit";
 
 export function Form() {
+  const [verified, setVerified] = useState(false);
+  const [captchaToken, setCaptchaToken] = useState("");
+
+  const turnstile = useTurnstile();
   return (
     <form
-      action={sendMail}
-      className="w-full h-full flex flex-col md:flex-row items-center justify-center gap-6 mt-10"
+      action={verified ? sendMail : undefined}
+      className="w-full h-full flex flex-col md:flex-row items-start justify-center gap-6 mt-10"
     >
       <div className="md:w-[40%] w-[95%] md:h-[300px] h-[40%] flex flex-col items-center gap-5 justify-start">
         <div className="h-fit w-full flex flex-col md:flex-row items-start md:items-center justify-start gap-5">
@@ -55,7 +61,8 @@ export function Form() {
           />
         </div>
       </div>
-      <input type="hidden" name="From" />
+
+      <input type="hidden" name="g-recaptcha-response" value={captchaToken} />
       <div className="md:w-[40%] w-[95%] md:h-[300px] h-fit flex flex-col items-center gap-5 justify-start">
         <div className="h-[60%] w-full flex flex-col md:flex-row items-start justify-start gap-5">
           <div className="w-[60%] md:w-[20%] h-full flex flex-col items-start justify-start">
@@ -71,8 +78,17 @@ export function Form() {
           />
         </div>
       </div>
-      <div className="md:w-[20%] w-[95%] md:h-[150px] h-[20%] flex flex-col items-center justify-end md:justify-start">
-        <SubmitButton />
+      <div className="md:w-fit shrink-0 gap-5 w-[95%] md:h-fit h-[20%] flex flex-col items-center justify-end md:justify-start">
+        <Turnstile
+          sitekey="0x4AAAAAAAgk0KI7mfH7t1k9"
+          fixedSize
+          onVerify={(token) => {
+            setVerified(true);
+            setCaptchaToken(token);
+            turnstile.reset();
+          }}
+        />
+        <SubmitButton verified={verified} />
       </div>
     </form>
   );
--- app/(contact)/submit.tsx
+++ app/(contact)/submit.tsx
@@ -1,15 +1,16 @@
 "use client";
 
-export function SubmitButton() {
+export function SubmitButton({ verified }: { verified: boolean }) {
   return (
     <button
+      disabled={!verified}
       type="submit"
       onClick={async () => {
         window.dispatchEvent(new Event("mailSent"));
       }}
       className="w-fit py-3 px-5 h-fit bg-brand-background-secondary text-white rounded-md"
     >
-      <p className="text-[20px]">Submit</p>
+      <p className="text-[20px]">{verified ? "Submit" : "Verify"}</p>
     </button>
   );
 }
