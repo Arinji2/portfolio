d26fb2aa825893799b98c6993f9ff9c2f15f3312
Arinji2 <arinjaydhar205@gmail.com>
2024-10-24 11:45:24 +0530

    cleanup experience

--- app/experience.tsx
+++ app/experience.tsx
@@ -20,11 +20,11 @@ export default async function Experience() {
       <WidthWrapper transparent>
         <div className="w-full h-full flex flex-col  items-center justify-start pt-10 pb-20 z-20">
           <div className=" md:pl-10 shrink-0 h-fit w-[95%]  xl:w-full  flex flex-row items-center gap-3 justify-center  z-10">
-            <h2 className="text-[40px] md:text-[50px] xl:text-[70px] text-brand-primary font-bold text-center">
+            <h2 className="text-brand-primary xl:text-5xl md:text-3xl text-2xl tracking-title font-bold">
               EXPERIENCE
             </h2>
           </div>
-          <div className="w-[95%] mt-auto xl:w-full gap-5 md:pl-10  h-fit flex flex-row items-start pt-10 no-scrollbar justify-center overflow-x-scroll overflow-y-hidden z-50">
+          <div className="w-[95%] mt-auto xl:w-full gap-5 md:pl-10  h-fit flex flex-row items-start pt-10 no-scrollbar justify-center overflow-x-auto overflow-y-hidden z-50">
             <TechBlock
               name="REVIVENODE"
               description="Premium servers at a pocket friendly price"
@@ -56,19 +56,21 @@ function TechBlock({
     <Link
       href={link}
       target="_blank"
-      className="hover:scale-90 scale-[.80] group hover:cursor-pointer will-change-transform transition-all ease-in-out duration-300 w-[300px] md:w-[400px] bg-brand-background-overlay bg-opacity-60 rounded-md relative shrink-0 h-[450px] flex flex-col items-center justify-center gap-5 text-center"
+      className="hover:scale-90 scale-[.80] group hover:cursor-pointer will-change-transform transition-all ease-in-out duration-300 w-[300px] md:w-[400px] bg-brand-background-overlay bg-opacity-60 rounded-md relative shrink-0 h-fit py-8 flex flex-col items-center justify-center gap-8 text-center"
     >
-      <h3 className="text-[30px] md:text-[40px] text-brand-primary font-bold">
+      <h3 className="xl:text-3xl md:text-2xl text-xl text-brand-primary font-bold">
         {name}
       </h3>
       <div className="w-[90%] h-fit flex flex-col items-center justify-center text-center gap-7">
-        <p className="text-white/60 transition-color ease-in-out duration-200 group-hover:text-white text-[15px] md:text-[20px] ">
+        <p className="text-white/60 transition-color ease-in-out duration-200 group-hover:text-white xl:text-xl md:text-lg text-base ">
           {description}
         </p>
-        <p className="text-[30px] md:text-[40px] text-white font-bold">
+        <p className=" text-white/80  xl:text-xl md:text-lg text-base font-bold">
           {post}
         </p>
-        <p className=" text-[15px] md:text-[20px] text-brand-primary">{time}</p>
+        <p className="  xl:text-xl md:text-lg text-base text-brand-primary">
+          {time}
+        </p>
       </div>
     </Link>
   );
